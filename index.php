708fa6aa53489824f4d7b43f01704242dc5164c5
Chrisvenator <55980003+Chrisvenator@users.noreply.github.com>
2020-05-04 13:38:38 +0200

    release 1.3.3

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Registrieren</title>
+</head>
+<body class="content">
+You have to create a new Virtual host with the name: "chrisvenator.hd"
+This page ist not longer avaliable. Please use this Link: <a href="http://chrisvenator.hd/Golf_Club_Arnold_Palmer/Remember/index.html">
+</body>
+</html>
